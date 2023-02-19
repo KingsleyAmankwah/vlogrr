5f7933bfaf6aab1453c966acc4b18e85d3730fd7
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-20 05:05:16 +0530

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
+import Sidenav from "../components/Sidenav";
 
-const Home = () => {
+function Home() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <>
-      <Navbar />
-    </>
+    <div>
+      <main className="bg-gray-200 w-screen h-screen ">
+        <div className="">
+          <Navbar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+          <div className="flex">
+            <Sidenav open={sidebarOpen} />
+            <div className="h-full w-full py-3 px-5"></div>
+          </div>
+        </div>
+      </main>
+    </div>
   );
-};
+}
 
 export default Home;
