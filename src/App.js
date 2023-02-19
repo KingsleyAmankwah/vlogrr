5b7a09c00ea583da8e165846e80469173d887f35
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-19 10:13:28 +0530

    App entry file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="login" element={<Navbar />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
