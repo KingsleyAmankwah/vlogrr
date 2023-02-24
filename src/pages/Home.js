c973ddfc6e7c0f622a788ee672482c03e4cc4a8e
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-24 20:47:24 +0530

    Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,34 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Create from "../components/Create";
 import Navbar from "../components/Navbar";
+import Post from "../components/Post";
 import Sidenav from "../components/Sidenav";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Feed from "../components/Feed";
 
 function Home() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <div>
-      <main className="bg-gray-200 w-screen h-screen ">
+      <main className="bg-gray-200 w-full h-full ">
         <div className="">
           <Navbar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
           <div className="flex">
             <Sidenav open={sidebarOpen} />
-            <div className="h-full w-full py-3 px-5"></div>
+            <div className="h-full w-full py-3 px-5">
+              <Routes>
+                <Route path="/" element={<Feed />} />
+                <Route path="/create" element={<Create />} />
+                <Route path="/post" element={<Post />} />
+              </Routes>
+            </div>
           </div>
         </div>
       </main>
+      <ToastContainer />
     </div>
   );
 }
