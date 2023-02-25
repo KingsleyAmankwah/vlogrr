d5ac8e08a0b4b4ba5fb63cf86488b6d27587e6f8
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-25 12:23:22 +0530

    Test file

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -13,122 +13,138 @@ const Sidebar = () => {
   const [location, setLocation] = useState("");
 
   return (
-    <form class="flex justify-center items-center w-full min-h-screen p-10">
-      <div class="w-4/5 h-full border border-gray-300 rounded-md p-4 flex flex-col items-center justify-center gap-2">
-        {/* {alert && (
-          <AlertMsg status={alertStatus} msg={alertMsg} icon={alertIcon} />
-        )} */}
+    // <form class="flex justify-center items-center w-full min-h-screen p-10">
+    //   <div class="w-4/5 h-full border border-gray-300 rounded-md p-4 flex flex-col items-center justify-center gap-2">
+    //     {/* {alert && (
+    //       <AlertMsg status={alertStatus} msg={alertMsg} icon={alertIcon} />
+    //     )} */}
 
-        <input
-          class="w-full text-lg placeholder-gray-500 outline-none border-b"
-          placeholder="Title"
-          required
-          type="text"
-          // value={title}
-          // onChange={(e) => setTitle(e.target.value)}
-        />
+    //     <input
+    //       class="w-full text-lg placeholder-gray-500 outline-none border-b"
+    //       placeholder="Title"
+    //       required
+    //       type="text"
+    //       // value={title}
+    //       // onChange={(e) => setTitle(e.target.value)}
+    //     />
 
-        <div class="w-full md:flex justify-between items-center my-4 gap-8">
-          <div class="w-full bg-blue-500 flex justify-between items-center my-4 gap-8">
-            <select class="w-full bg-blue-500 text-white outline-none py-2 px-4 rounded-md">
-              <option disabled selected class=" py-2 px-4 rounded-md">
-                {category}
-              </option>
-              {categories &&
-                categories.map((data) => (
-                  <option
-                    key={data.id}
-                    class="px-4 py-2 text-lg hover:bg-black hover:bg-opacity-25"
-                    onClick={() => setCategory(data.name)}
-                    value={data.name}
-                  >
-                    {data.iconSrc}
-                    <span class="ml-4">{data.name}</span>
-                  </option>
-                ))}
-            </select>
-          </div>
-          {/* <div class="relative">
-            <button class="w-full bg-blue-500 text-white py-2 px-4 rounded-md">
-              {category}
-            </button>
-            <div class="absolute top-full left-0 z-10 w-64 shadow-xl">
-              <ul class="bg-white divide-y divide-gray-200">
-                {categories &&
-                  categories.map((data) => (
-                    <li
-                      key={data.id}
-                      class="px-4 py-2 text-lg hover:bg-black hover:bg-opacity-25"
-                      onClick={() => setCategory(data.name)}
-                    >
-                      {data.iconSrc}
-                      <span class="ml-4">{data.name}</span>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-          </div> */}
+    //     <div class="w-full md:flex justify-between items-center my-4 gap-8">
+    //       <div class="w-full bg-blue-500 flex justify-between items-center my-4 gap-8">
+    //         <select class="w-full bg-blue-500 text-white outline-none py-2 px-4 rounded-md">
+    //           <option disabled selected class=" py-2 px-4 rounded-md">
+    //             {category}
+    //           </option>
+    //           {categories &&
+    //             categories.map((data) => (
+    //               <option
+    //                 key={data.id}
+    //                 class="px-4 py-2 text-lg hover:bg-black hover:bg-opacity-25"
+    //                 onClick={() => setCategory(data.name)}
+    //                 value={data.name}
+    //               >
+    //                 {data.iconSrc}
+    //                 <span class="ml-4">{data.name}</span>
+    //               </option>
+    //             ))}
+    //         </select>
+    //       </div>
+    //       {/* <div class="relative">
+    //         <button class="w-full bg-blue-500 text-white py-2 px-4 rounded-md">
+    //           {category}
+    //         </button>
+    //         <div class="absolute top-full left-0 z-10 w-64 shadow-xl">
+    //           <ul class="bg-white divide-y divide-gray-200">
+    //             {categories &&
+    //               categories.map((data) => (
+    //                 <li
+    //                   key={data.id}
+    //                   class="px-4 py-2 text-lg hover:bg-black hover:bg-opacity-25"
+    //                   onClick={() => setCategory(data.name)}
+    //                 >
+    //                   {data.iconSrc}
+    //                   <span class="ml-4">{data.name}</span>
+    //                 </li>
+    //               ))}
+    //           </ul>
+    //         </div>
+    //       </div> */}
 
-          <div class="w-full">
-            <div class="relative">
-              <div class="absolute left-0 top-0 h-full w-10 flex items-center justify-center">
-                <IoLocation />
-              </div>
-              <input
-                class="w-full pl-12 pr-4 py-2 text-lg placeholder-gray-500 border border-gray-300 outline-none rounded-md"
-                placeholder="Location"
-                type="text"
-                // value={url}
-                // onChange={(e) => setUrl(e.target.value)}
-              />
-            </div>
+    //       <div class="w-full">
+    //         <div class="relative">
+    //           <div class="absolute left-0 top-0 h-full w-10 flex items-center justify-center">
+    //             <IoLocation />
+    //           </div>
+    //           <input
+    //             class="w-full pl-12 pr-4 py-2 text-lg placeholder-gray-500 border border-gray-300 outline-none rounded-md"
+    //             placeholder="Location"
+    //             type="text"
+    //             // value={url}
+    //             // onChange={(e) => setUrl(e.target.value)}
+    //           />
+    //         </div>
+    //       </div>
+    //     </div>
+
+    //     <div class="w-full h-64 flex justify-center items-center">
+    //       {!videoAsset ? (
+    //         <>
+    //           <label for="file-upload" class="cursor-pointer flex items-center">
+    //             <span class="mr-2">
+    //               <IoCloudUpload />
+    //             </span>
+    //             <span class="text-gray-500 font-medium">Choose a file</span>
+    //           </label>
+    //           <input
+    //             id="file-upload"
+    //             type="file"
+    //             class="hidden"
+    //             onChange={(e) =>
+    //               setVideoAsset(URL.createObjectURL(e.target.files[0]))
+    //             }
+    //           />
+    //         </>
+    //       ) : (
+    //         <video
+    //           src={videoAsset}
+    //           controls
+    //           autoplay
+    //           loop
+    //           class="w-full h-full"
+    //         ></video>
+    //       )}
+    //     </div>
+
+    //     <textarea
+    //       class="w-full h-64 resize-none border border-gray-300 rounded-md p-2 text-lg placeholder-gray-500 outline-none"
+    //       placeholder="Description"
+    //       // value={description}
+    //       // onChange={(e) => setDescription(e.target.value)}
+    //     ></textarea>
+
+    //     <button
+    //       class="w-full bg-blue-500 text-white py-2 px-4 rounded-md mt-4 hover:bg-blue-600"
+    //       // onClick={handleSubmit}
+    //     >
+    //       Submit
+    //     </button>
+    //   </div>
+    // </form>
+    <>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div class="relative">
+          <video
+            src="your-video-source"
+            class="w-full h-full object-cover rounded-lg shadow-md"
+          ></video>
+          <div class="absolute top-0 left-0 m-2">
+            <span class="bg-gray-200 text-gray-700 rounded-full px-2 py-1 text-xs font-bold">
+              10:23
+            </span>
           </div>
         </div>
-
-        <div class="w-full h-64 flex justify-center items-center">
-          {!videoAsset ? (
-            <>
-              <label for="file-upload" class="cursor-pointer flex items-center">
-                <span class="mr-2">
-                  <IoCloudUpload />
-                </span>
-                <span class="text-gray-500 font-medium">Choose a file</span>
-              </label>
-              <input
-                id="file-upload"
-                type="file"
-                class="hidden"
-                onChange={(e) =>
-                  setVideoAsset(URL.createObjectURL(e.target.files[0]))
-                }
-              />
-            </>
-          ) : (
-            <video
-              src={videoAsset}
-              controls
-              autoplay
-              loop
-              class="w-full h-full"
-            ></video>
-          )}
-        </div>
-
-        <textarea
-          class="w-full h-64 resize-none border border-gray-300 rounded-md p-2 text-lg placeholder-gray-500 outline-none"
-          placeholder="Description"
-          // value={description}
-          // onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
-
-        <button
-          class="w-full bg-blue-500 text-white py-2 px-4 rounded-md mt-4 hover:bg-blue-600"
-          // onClick={handleSubmit}
-        >
-          Submit
-        </button>
+        {/* <!-- Repeat the above video block for all videos in your Firestore database --> */}
       </div>
-    </form>
+    </>
   );
 };
 
