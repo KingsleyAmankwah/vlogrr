db5ed952a69652b39ffbfb9e20325b1f863625f0
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-02-28 20:19:16 +0530

    post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,18 +5,19 @@ import { FaSmileWink } from "react-icons/fa";
 // import { AiOutlineLogout } from "react-icons/ai";
 import { IoCloudUpload, IoLocation } from "react-icons/io5";
 import Spinner from "./Spinner";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-  deleteObject,
-} from "firebase/storage";
+// import {
+// getStorage,
+// ref,
+// uploadBytesResumable,
+// getDownloadURL,
+// deleteObject,
+// } from "firebase/storage";
 import { firebaseApp } from "../firebase-config";
-import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { doc, getFirestore, setDoc, serverTimestamp } from "firebase/firestore";
 import { categories } from "../data";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 
 const Post = () => {
   const [category, setCategory] = useState("Select category");
@@ -26,10 +27,16 @@ const Post = () => {
   const [location, setLocation] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const storage = getStorage(firebaseApp);
+  // const storage = getStorage(firebaseApp);
   const firestoreDb = getFirestore(firebaseApp);
 
   const navigate = useNavigate();
+
+  const auth = getAuth();
+
+  const handleRemoveVideo = () => {
+    setVideoAsset(null);
+  };
 
   const uploadVideo = async (e) => {
     e.preventDefault();
@@ -39,18 +46,23 @@ const Post = () => {
         return;
       }
 
-      setLoading(true); // Set loading to true here
+      const user = auth.currentUser;
+      if (!user) {
+        throw new Error("User not authenticated");
+      }
 
       const data = {
         id: `${Date.now()}`,
         title: title,
-        // userId: userInfo?.uid,
+        userId: user.uid,
         category: category,
         location: location,
         videoUrl: videoAsset,
         description: description,
+        timestamp: serverTimestamp(),
       };
 
+      setLoading(true); // Set loading to true here
       // Upload the video
       await setDoc(doc(firestoreDb, "videos", `${Date.now()}`), data);
 
@@ -59,30 +71,25 @@ const Post = () => {
       setLocation("");
       setDescription("");
       setVideoAsset("");
+      navigate("/");
       toast.success("Video uploaded!!!");
     } catch (error) {
       console.log(error);
       toast.error("Error uploading video");
-    } finally {
-      setLoading(false); // Set loading to false here regardless of whether there was an error or not
+      setLoading(false);
     }
   };
 
   return (
-    <form class="flex justify-center items-center w-full min-h-screen p-10">
-      <div class="w-4/5 md:w-full h-full border border-gray-300 rounded-md p-4 flex flex-col items-center justify-center gap-2">
-        {/* {alert && (
-          <AlertMsg status={alertStatus} msg={alertMsg} icon={alertIcon} />
-        )} */}
-
-        <div class="w-full">
-          <div class="relative">
-            <div class="absolute left-0 top-0 h-full w-10 flex items-center justify-center">
-              {/* <IoLocation /> */}
+    <form className="flex justify-center items-center w-full min-h-screen p-10">
+      <div className="w-4/5 md:w-full h-full border border-gray-300 rounded-md p-4 flex flex-col items-center justify-center gap-2">
+        <div className="w-full">
+          <div className="relative">
+            <div className="absolute left-0 top-0 h-full w-10 flex items-center justify-center">
               <FaSmileWink />
             </div>
             <input
-              class="w-full pl-12 pr-4 py-2 text-lg placeholder-gray-500 border border-gray-300 outline-none rounded-md"
+              className="w-full pl-12 pr-4 py-2 text-lg placeholder-gray-500 border border-gray-300 outline-none rounded-md"
               placeholder="Title"
               type="text"
               value={title}
@@ -91,68 +98,38 @@ const Post = () => {
           </div>
         </div>
 
-        {/* <input
-          class="w-full text-lg placeholder-gray-500 outline-none border-b"
-          placeholder="Title"
-          required
-          type="text"
-          // value={title}
-          // onChange={(e) => setTitle(e.target.value)}
-        /> */}
-
-        <div class="w-full md:flex justify-between items-center my-4 gap-8">
-          <div class="w-full bg-blue-500 flex justify-between items-center my-4 gap-8">
+        <div className="w-full md:flex justify-between items-center my-4 gap-8">
+          <div className="w-full bg-blue-500 flex justify-between items-center my-4 gap-8">
             <select
-              class="w-full bg-blue-500 text-white outline-none py-2 px-4 rounded-md"
+              className="w-full bg-blue-500 text-white outline-none py-2 px-4 rounded-md"
               value={category}
               onChange={(e) => setCategory(e.target.value)}
             >
-              <option disabled selected class="py-2 px-4 rounded-md">
+              <option disabled defaultValue className="py-2 px-4 rounded-md">
                 Select category
               </option>
               {categories &&
                 categories.map((data) => (
                   <option
                     key={data.id}
-                    class="px-4 py-2 text-lg hover:bg-black hover:bg-opacity-25"
+                    className="p-2 text-lg hover:bg-black hover:bg-opacity-25"
                     onClick={() => setCategory(data.name)}
                     value={data.name}
                   >
-                    {data.iconSrc}
-                    <span class="ml-4">{data.name}</span>
+                    {/* {data.iconSrc} */}
+                    {data.name}
                   </option>
                 ))}
             </select>
           </div>
 
-          {/* <div class="relative">
-            <button class="w-full bg-blue-500 text-white py-2 px-4 rounded-md">
-              {category}
-            </button>
-            <div class="absolute top-full left-0 z-10 w-64 shadow-xl">
-              <ul class="bg-white divide-y divide-gray-200">
-                {categories &&
-                  categories.map((data) => (
-                    <li
-                      key={data.id}
-                      class="px-4 py-2 text-lg hover:bg-black hover:bg-opacity-25"
-                      onClick={() => setCategory(data.name)}
-                    >
-                      {data.iconSrc}
-                      <span class="ml-4">{data.name}</span>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-          </div> */}
-
-          <div class="w-full">
-            <div class="relative">
-              <div class="absolute left-0 top-0 h-full w-10 flex items-center justify-center">
+          <div className="w-full">
+            <div className="relative">
+              <div className="absolute left-0 top-0 h-full w-10 flex items-center justify-center">
                 <IoLocation />
               </div>
               <input
-                class="w-full pl-12 pr-4 py-2 text-lg placeholder-gray-500 border border-gray-300 outline-none rounded-md"
+                className="w-full pl-12 pr-4 py-2 text-lg placeholder-gray-500 border border-gray-300 outline-none rounded-md"
                 placeholder="Location"
                 type="text"
                 value={location}
@@ -162,7 +139,55 @@ const Post = () => {
           </div>
         </div>
 
-        <div class="w-full h-64 flex justify-center items-center">
+        <div className="w-full h-full flex justify-center items-center">
+          {!videoAsset ? (
+            <>
+              {loading ? (
+                <Spinner />
+              ) : (
+                <>
+                  <label
+                    htmlFor="file-upload"
+                    className="cursor-pointer flex items-center"
+                  >
+                    <span className="mr-2">
+                      <IoCloudUpload />
+                    </span>
+                    <span className="text-gray-500 font-medium">
+                      Choose a file
+                    </span>
+                  </label>
+                  <input
+                    id="file-upload"
+                    type="file"
+                    className="hidden"
+                    onChange={(e) =>
+                      setVideoAsset(URL.createObjectURL(e.target.files[0]))
+                    }
+                  />
+                </>
+              )}
+            </>
+          ) : (
+            <div className="relative">
+              <video
+                src={videoAsset}
+                controls
+                autoPlay
+                loop
+                className="w-full h-full"
+              ></video>
+              <button
+                className="absolute top-2 right-2 bg-red-500 text-white px-2 py-1 rounded-md"
+                onClick={handleRemoveVideo}
+              >
+                X
+              </button>
+            </div>
+          )}
+        </div>
+
+        {/* <div className="w-full h-64 flex justify-center items-center">
           {!videoAsset ? (
             <>
               {loading ? (
@@ -171,20 +196,20 @@ const Post = () => {
                 <>
                   <label
                     for="file-upload"
-                    class="cursor-pointer flex items-center"
+                    className="cursor-pointer flex items-center"
                   >
-                    <span class="mr-2">
+                    <span className="mr-2">
                       <IoCloudUpload />
                     </span>
-                    <span class="text-gray-500 font-medium">Choose a file</span>
+                    <span className="text-gray-500 font-medium">
+                      Choose a file
+                    </span>
                   </label>
                   <input
                     id="file-upload"
                     type="file"
-                    class="hidden"
-                    onChange={(e) =>
-                      setVideoAsset(URL.createObjectURL(e.target.files[0]))
-                    }
+                    className="hidden"
+                  
                   />
                 </>
               )}
@@ -195,22 +220,22 @@ const Post = () => {
               controls
               autoplay
               loop
-              class="w-full h-full"
+              className="w-full h-full"
             ></video>
           )}
-        </div>
+        </div> */}
 
         <textarea
-          class="w-full h-64 resize-none border border-gray-300 rounded-md p-2 text-lg placeholder-gray-500 outline-none"
+          className="w-full h-64 resize-none border border-gray-300 rounded-md p-2 text-lg placeholder-gray-500 outline-none"
           placeholder="Description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         ></textarea>
 
         <button
-          class="w-full bg-blue-500 text-white py-2 px-4 rounded-md mt-4 hover:bg-blue-600"
+          className="w-full disabled:bg-blue-200 bg-blue-500 text-white py-2 px-4 rounded-md mt-4 hover:bg-blue-600"
           onClick={uploadVideo}
-          // type="submit"
+          disabled={loading}
         >
           Submit
         </button>
